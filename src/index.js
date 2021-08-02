b4dc61be5c3da61c52abfd5e70f1df19c4a91260
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 14:07:08 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -15,12 +15,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(authRoutes);
 app.use(trackRoutes);
 
-app.use(function(req, res, next) {
-    res.headers("Access-Control-Allow-Origin", req.headers('origin'));
-    res.headers("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.headers("Access-Control-Allow-Credentials","true");
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://trackbackend.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-  }); 
+}); 
 
 const mongoUri = 'mongodb+srv://adminNir:n12345@cluster0.bnsjt.mongodb.net/tracker?retryWrites=true&w=majority';
 
