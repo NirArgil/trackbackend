1270643f991f1f1724d6330039d325339ac2bf7e
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 21:52:13 +0300

    commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization'); 
 
     res.send('cors problem fixed:)');
 });
