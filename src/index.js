2d6476bff56d2651f6e87d1fc451601ec01d51ab
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 22:40:38 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(cors());
 
 app.use(express.json());
