37dbae7fab9ac040bf88bac5967db6fe59e9678a
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 21:58:38 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use('/', function(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization'); 
 
-    res.send('cors problem fixed:)');
+    res.send('cors problem fixed111');
 });
 
 
