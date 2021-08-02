2036ce191c745a6d26c6edf2ad6d9793be6313bf
NIR <MAILMAIL10@GMAIL.COM>
2021-08-03 01:26:05 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://trackbackend.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
   });
 
