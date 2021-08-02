6ef36d975a0f8796a5a8d4d8b36e104472699ef0
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 14:12:15 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(trackRoutes);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://trackbackend.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
