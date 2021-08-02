616af538e4a965a631fc02cd0ac83d7d4ce8c6a6
NIR <MAILMAIL10@GMAIL.COM>
2021-08-02 22:57:26 +0300

    commit

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 module.exports = (req, res, next) => {
-    const { authorization } = req.headers;
+    const authorization  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MGZmZWExNzk2YmQ2NzFjZTBjMjRlODgiLCJpYXQiOjE2Mjc5MzQxODN9.R2YwZrvg-YvVlzbG5l_6eYNDzCgi-YwRxgb2crexoqg';
 
     if (!authorization) {
         return res.status(401).send({ error: 'No authorization'});
